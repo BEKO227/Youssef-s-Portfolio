51163eb2887b55c6fbe5842b428ab9170297cc44
Youssef Hashish <yousfhashish0@gmail.com>
2025-09-28 16:43:30 +0300

    ig link update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,8 +9,8 @@ export default function page() {
           <h1 className="text-8xl font-bold text-yellow-500">Hello</h1>
           <Typewriter className="text-yellow-500"/>
             <div className="text-white font-bold">
-                <a href="https://www.facebook.com/youssef.hashish.5" className="m-5 icon fab fa-facebook hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="Facebook"></a>
-                <a href="https://www.instagram.com/yousfhashish/" className="m-5 icon fab fa-instagram hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="Instagram"></a>
+                <a href="https://www.facebook.com/share/1BHdESNLmr/?mibextid=wwXIfr" className="m-5 icon fab fa-facebook hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="Facebook"></a>
+                <a href="https://www.instagram.com/yousfhashish?igsh=eG11NGRkZmQwaHV5&utm_source=qr" className="m-5 icon fab fa-instagram hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="Instagram"></a>
                 <a href="https://github.com/BEKO227" className="m-5 icon fab fa-github hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="GitHub"></a>
                 <a href="https://www.linkedin.com/in/yousf-hashish-567a16211/" className="m-5 icon fab fa-linkedin hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="LinkedIn"></a>
                 <a href="https://drive.google.com/file/d/148eHFu3VwYepfHq2BT16hmvW-SUqLBgr/view?usp=drive_link" className="m-5 icon fa-solid fa-file hover:text-yellow-500" target="_blank" rel="noopener noreferrer" title="Download CV"></a>
